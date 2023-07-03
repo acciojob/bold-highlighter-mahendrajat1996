8c720520a00e006702d61f7617d677758f5b8702
mahendrajat1996 <130475100+mahendrajat1996@users.noreply.github.com>
2023-07-03 22:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-function highlight() {
-    //Write your code here
+// function highlight() {
+//     //Write your code here
 
 
-}
+// }
 
 
-function return_normal() {
-    //Write your code here
+// function return_normal() {
+//     //Write your code here
 
     
+// }
+function highlight() {
+  // Get all the <strong> elements within the paragraph
+  const boldWords = document.querySelectorAll('p strong');
+
+  // Change the color of bold words to green
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = 'green';
+  }
+}
+
+function return_normal() {
+  // Get all the <strong> elements within the paragraph
+  const boldWords = document.querySelectorAll('p strong');
+
+  // Revert the color of bold words back to black
+  for (let i = 0; i < boldWords.length; i++) {
+    boldWords[i].style.color = 'black';
+  }
 }
